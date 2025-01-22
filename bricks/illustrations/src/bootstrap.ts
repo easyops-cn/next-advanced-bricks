92497ce14aa4cce2badd38ac86f3b217835a25ce
Wang Shenwei <stevewang@easyops.cn>
2025-01-22 15:04:30 +0800

    chore(): move out illustrations

--- bricks/illustrations/src/bootstrap.ts
+++
@@ -1,4 +0,0 @@
-import "./data-providers/get-illustration.js";
-import "./data-providers/get-illustrations-by-category.js";
-import "./loading-transition/index.js";
-import "./illustration-message/index.js";
