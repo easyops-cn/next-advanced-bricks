fb59d66cc2f42d07b4c6c95be2fcfc7d52778c3c
Wang Shenwei <stevewang@easyops.cn>
2025-02-20 12:18:55 +0800

    refactor(): use canvas

--- bricks/mini-chart/src/mini-line-chart/index.tsx
+++ bricks/mini-chart/src/mini-line-chart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useEffect } from "react";
 import { createDecorators } from "@next-core/element";
 import { ReactNextElement } from "@next-core/react-element";
 import { useTranslation, initializeReactI18n } from "@next-core/i18n/react";
@@ -8,6 +8,8 @@ import styleText from "./styles.shadow.css";
 import { K, NS, locales } from "./i18n.js";
 
 initializeReactI18n(NS, locales);
+
+const PIXEL_RATIO = window.devicePixelRatio ?? 1;
 
 const { defineElement, property } = createDecorators();
 
@@ -103,10 +105,35 @@ export function MiniLineChartComponent({
 
   const { t } = useTranslation(NS);
 
-  const d = useMemo(() => {
-    if (!data?.length) {
-      return undefined;
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const detectorRef = React.useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const detector = detectorRef.current;
+    if (!canvas || !detector) {
+      return;
     }
+
+    const ctx = canvas.getContext("2d")!;
+    ctx.save();
+    ctx.scale(PIXEL_RATIO, PIXEL_RATIO);
+    ctx.clearRect(0, 0, width, height);
+
+    if (!data?.length) {
+      ctx.restore();
+      return;
+    }
+
+    ctx.lineWidth = 2;
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+    if (lineColor) {
+      ctx.strokeStyle = getComputedStyle(detector).color;
+    }
+
+    ctx.beginPath();
+    ctx.translate(padding, padding);
 
     let min = Infinity;
     let max = -Infinity;
@@ -120,28 +147,47 @@ export function MiniLineChartComponent({
       }
     }
 
+    let path: Point[];
+
     if (min === max) {
       const y = min === 0 ? innerHeight : innerHeight / 2;
-      return `M${0},${y}L${innerWidth},${y}`;
+      path = [
+        [0, y],
+        [innerWidth, y],
+      ];
+    } else {
+      const start = data[0][xField];
+      const end = data[data.length - 1][xField];
+      const xScale = innerWidth / (end - start);
+      const yScale = innerHeight / (max - min);
+
+      path = data.map<Point>((item) => {
+        const x = (item[xField] - start) * xScale;
+        const y = innerHeight! - (item[yField] - min) * yScale;
+        return [x, y];
+      });
     }
-
-    const start = data[0][xField];
-    const end = data[data.length - 1][xField];
-    const xScale = innerWidth / (end - start);
-    const yScale = innerHeight / (max - min);
-
-    const path = data.map<Point>((item) => {
-      const x = (item[xField] - start) * xScale;
-      const y = innerHeight! - (item[yField] - min) * yScale;
-      return [x, y];
-    });
 
     // Keep smooth behavior as G2 line chart implementation
     // See https://github.com/antvis/G2/blob/6013d72881276aca9d17d93908d33b21194979c6/src/shape/line/smooth.ts#L20
-    return line().curve(smooth === false ? curveLinear : curveMonotoneX)(path)!;
-  }, [data, innerHeight, innerWidth, smooth, xField, yField]);
+    line()
+      .context(ctx)
+      .curve(smooth === false ? curveLinear : curveMonotoneX)(path);
+    ctx.stroke();
+    ctx.restore();
+  }, [
+    data,
+    height,
+    innerHeight,
+    innerWidth,
+    lineColor,
+    smooth,
+    width,
+    xField,
+    yField,
+  ]);
 
-  if (!d) {
+  if (!data?.length) {
     // No data
     return (
       <div style={{ width, height }}>
@@ -151,17 +197,18 @@ export function MiniLineChartComponent({
   }
 
   return (
-    <svg width={width} height={height}>
-      <g transform={`translate(${padding},${padding})`}>
-        <path
-          d={d}
-          fill="none"
-          stroke={lineColor}
-          strokeWidth={2}
-          strokeLinejoin="round"
-          strokeLinecap="round"
-        />
-      </g>
-    </svg>
+    <>
+      <canvas
+        width={width * PIXEL_RATIO}
+        height={height * PIXEL_RATIO}
+        ref={canvasRef}
+        style={{ width, height }}
+      />
+      <span
+        className="detector"
+        ref={detectorRef}
+        style={{ color: lineColor }}
+      ></span>
+    </>
   );
 }
