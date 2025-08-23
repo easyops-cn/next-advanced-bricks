89dda397381999cd110a532a07a82edc1294da14
Wang Shenwei <stevewang@easyops.cn>
2025-08-24 00:52:23 +0800

    fix(): support compatible languages

--- shared/monaco-textmate/src/TokensProvider.ts
+++ shared/monaco-textmate/src/TokensProvider.ts
@@ -5,12 +5,18 @@ import wasm from "vscode-oniguruma/release/onig.wasm";
 import { registerBrickNextYaml } from "./brick_next_yaml.js";
 
 const languageToScope = new Map();
-languageToScope.set("javascript", "source.jsx");
+languageToScope.set("javascript", "source.js.jsx");
 languageToScope.set("typescript", "source.tsx");
 languageToScope.set("html", "text.html.basic");
+languageToScope.set("markdown", "text.html.markdown");
+languageToScope.set("xml", "text.xml");
 languageToScope.set("css", "source.css");
 languageToScope.set("json", "source.json");
 languageToScope.set("python", "source.python");
+languageToScope.set("go", "source.go");
+languageToScope.set("java", "source.java");
+languageToScope.set("shell", "source.shell");
+languageToScope.set("powershell", "source.powershell");
 languageToScope.set("yaml", "source.yaml");
 languageToScope.set("brick_next_yaml", "source.brick_next_yaml");
 
@@ -18,13 +24,20 @@ registerBrickNextYaml(monaco);
 
 const grammars = new Map([
   ["source.js", "JavaScript.tmLanguage"],
-  ["source.jsx", "JavaScriptReact.tmLanguage"],
+  ["source.js.jsx", "JavaScriptReact.tmLanguage"],
   ["source.ts", "TypeScript.tmLanguage"],
   ["source.tsx", "TypeScriptReact.tmLanguage"],
   ["text.html.basic", "html.tmLanguage"],
+  ["text.html.derivative", "html-derivative.tmLanguage"],
+  ["text.html.markdown", "markdown.tmLanguage"],
+  ["text.xml", "xml.tmLanguage"],
   ["source.css", "css.tmLanguage"],
   ["source.json", "JSON.tmLanguage"],
   ["source.python", "MagicPython.tmLanguage"],
+  ["source.go", "go.tmLanguage"],
+  ["source.java", "java.tmLanguage"],
+  ["source.shell", "shell-unix-bash.tmLanguage"],
+  ["source.powershell", "powershell.tmLanguage"],
   ["source.yaml", "yaml.tmLanguage"],
   ["source.yaml.1.0", "yaml-1.0.tmLanguage"],
   ["source.yaml.1.1", "yaml-1.1.tmLanguage"],
@@ -67,7 +80,11 @@ const registry = new vsctm.Registry({
       }
       return grammar;
     }
-    throw new Error(`No grammar found for scope: ${scopeName}`);
+    if (process.env.NODE_ENV !== "production") {
+      // eslint-disable-next-line no-console
+      console.warn(`No grammar found for scope: ${scopeName}`);
+    }
+    return null;
   },
   getInjections(scopeName) {
     if (scopeName === "source.brick_next_yaml") {
