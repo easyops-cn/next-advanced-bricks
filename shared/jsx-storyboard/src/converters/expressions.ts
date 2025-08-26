2a8ae2d7b6074b447dd837d15c2995131137e0a1
Wang Shenwei <stevewang@easyops.cn>
2025-08-26 11:50:04 +0800

    fix(): refine regexp for checking expression string

--- shared/jsx-storyboard/src/converters/expressions.ts
+++ shared/jsx-storyboard/src/converters/expressions.ts
@@ -1,19 +1,14 @@
 import { strictCollectMemberUsage } from "@next-core/utils/storyboard";
 
-const VALID_IDENTIFIER_REG = /^[$_\p{ID_Start}][$\p{ID_Continue}]*$/u;
+const EXPRESSION_PREFIX_REG = /^<%=?\s/;
+const EXPRESSION_SUFFIX_REG = /\s%>$/;
 
-const EXPRESSION_PREFIX_REG = /^\s*<%=?\s+/;
-const EXPRESSION_SUFFIX_REG = /\s+%>\s*$/;
-
-export function isExpression(value: string) {
+/**
+ * @param value The trimmed string content
+ * @returns if the value is a valid expression
+ */
+function isExpression(value: string) {
   return EXPRESSION_PREFIX_REG.test(value) && EXPRESSION_SUFFIX_REG.test(value);
-}
-
-export function getMemberAccessor(property: unknown): string {
-  const propertyStr = String(property);
-  return VALID_IDENTIFIER_REG.test(propertyStr)
-    ? `.${propertyStr}`
-    : `[${JSON.stringify(propertyStr)}]`;
 }
 
 export function parseDataSource(data: string | object):
@@ -35,29 +30,24 @@ export function parseDataSource(data: string | object):
     };
   }
 
-  let embedded: string;
   let expression: string;
-
-  if (isExpression(data)) {
-    expression = data
+  const expr = data.trim();
+  if (isExpression(expr)) {
+    expression = expr
       .replace(EXPRESSION_PREFIX_REG, "")
       .replace(EXPRESSION_SUFFIX_REG, "")
       .trim();
-
-    embedded = data;
-  } else if (data.includes("CTX.")) {
-    expression = data;
-    embedded = `<%= ${data} %>`;
   } else {
-    expression = `CTX${getMemberAccessor(data)}`;
-    embedded = `<%= ${expression} %>`;
+    // eslint-disable-next-line no-console
+    console.warn("Invalid expression:", JSON.stringify(data));
+    expression = "null";
   }
 
-  const usedContexts = strictCollectMemberUsage(embedded, "CTX");
+  const usedContexts = strictCollectMemberUsage(data, "CTX");
 
   return {
     isString: true,
-    embedded,
+    embedded: data,
     expression,
     usedContexts,
   };
