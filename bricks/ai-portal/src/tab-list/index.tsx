2577c59fc4c31d4b1277499c3d7e5d5516fe89f4
Wang Shenwei <stevewang@easyops.cn>
2025-09-29 17:36:55 +0800

    fix(): sticky tabs

--- bricks/ai-portal/src/tab-list/index.tsx
+++ bricks/ai-portal/src/tab-list/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createDecorators, type EventEmitter } from "@next-core/element";
 import { ReactNextElement } from "@next-core/react-element";
 import "@next-core/theme";
@@ -10,6 +10,8 @@ const { defineElement, property, event } = createDecorators();
 export interface TabListProps {
   tabs?: Tab[];
   activeTab?: string;
+  sticky?: boolean;
+  stickyThreshold?: number;
 }
 
 export interface Tab {
@@ -39,8 +41,20 @@ class TabList extends ReactNextElement implements TabListProps {
   @property({ attribute: false })
   accessor tabs: Tab[] | undefined;
 
-  @property({ attribute: false })
+  @property()
   accessor activeTab: string | undefined;
+
+  @property({ type: Boolean })
+  accessor sticky: boolean | undefined;
+
+  /**
+   * When the element's bounding client rect top is less than threshold,
+   * enabled sticky active style.
+   *
+   * @default 16
+   */
+  @property({ type: Number })
+  accessor stickyThreshold: number | undefined;
 
   @event({ type: "tab.click" })
   accessor #tabClick!: EventEmitter<Tab>;
@@ -55,23 +69,51 @@ class TabList extends ReactNextElement implements TabListProps {
       <TabListComponent
         tabs={this.tabs}
         activeTab={this.activeTab}
+        sticky={this.sticky}
+        stickyThreshold={this.stickyThreshold}
         onTabClick={this.#handleTabClick}
+        root={this}
       />
     );
   }
 }
 
 interface TabListComponentProps extends TabListProps {
+  root: HTMLElement;
   onTabClick: (tab: Tab) => void;
 }
 
 function TabListComponent({
   tabs,
   activeTab,
+  sticky,
+  stickyThreshold: _stickyThreshold,
+  root,
   onTabClick,
 }: TabListComponentProps) {
+  const [stickyActive, setStickyActive] = useState(false);
+  const stickyThreshold = _stickyThreshold ?? 16;
+
+  useEffect(() => {
+    setStickyActive(false);
+    if (!sticky) {
+      return;
+    }
+    const parent = getVerticalScrollParent(root);
+    if (parent) {
+      const onScroll = () => {
+        const rect = root.getBoundingClientRect();
+        setStickyActive(rect.top < stickyThreshold);
+      };
+      parent.addEventListener("scroll", onScroll);
+      return () => {
+        parent.removeEventListener("scroll", onScroll);
+      };
+    }
+  }, [sticky, stickyThreshold, root]);
+
   return (
-    <ul className="tabs" part="tabs">
+    <ul className={classNames("tabs", { sticky: stickyActive })} part="tabs">
       {tabs?.map((tab) => (
         <li
           key={tab.id}
@@ -83,4 +125,30 @@ function TabListComponent({
       ))}
     </ul>
   );
+}
+
+function getVerticalScrollParent(element: Element): Element | null {
+  if (!element) return null;
+
+  let parent = element.parentNode;
+
+  while (parent) {
+    if (parent instanceof ShadowRoot) {
+      parent = parent.host;
+    }
+    if (!(parent instanceof Element)) {
+      break;
+    }
+    const style = getComputedStyle(parent);
+    const overflowY = style.overflowY;
+    const isScrollableY = overflowY === "auto" || overflowY === "scroll";
+
+    if (isScrollableY) {
+      return parent;
+    }
+    parent = parent.parentNode;
+  }
+
+  // If none found, fallback to window
+  return document.scrollingElement || document.documentElement;
 }
